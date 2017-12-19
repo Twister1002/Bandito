953f59462891fc624631a0e718b2a4c3a26415da
Twister <twister1002@gmail.com>
2017-12-18 23:27:35 -0500

    Adjusted tabbing and added more packages. Added new files.

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const emailNotification = {
+    "title": "New emails",
+    "body": "You have new emails",
+    "icon": "" //path.join(__dirname, "../assets/assets/images/email.png")
+};
+
+
+const notification = new window.Notification(emailNotification.title, emailNotification);
